d1c9ba1b2ad32ad0e86725496f5d2f8a366d740a
Oluwatobiloba <iamtobihunkuten@gmail.com>
2022-06-23 14:39:55 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var choice = prompt("Enter Choice: +,-,/,*: ");
+var num1 = parseFloat(prompt("Enter First Number: "));
+var num2 = parseFloat(prompt("Enter Second number: "));
+
+
+var result;
+
+if (choice == '+'){
+    result = num1 + num2;
+} else if (choice == '-'){
+    result = num1 - num2;
+} else if (choice == '/'){
+    result == num1 / num2;
+} else if (choice == '*'){
+    result = num1 * num2;
+} else{
+    result = 'Invalid';  
+}
+alert(result)
